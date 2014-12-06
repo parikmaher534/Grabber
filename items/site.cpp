bdf47e430c52b17ea34aa5ba2c5c615032a28401
Nikolay Podstrigan <parikmaher534@gmail.com>
2014-12-06 19:31:12 +0300

    Update project class

--- items/site.cpp
+++ items/site.cpp
@@ -1,5 +1,3 @@
-#include <regex>
-
 class Project 
 {
 	public:
@@ -9,6 +7,7 @@ class Project
 		vector<Page*> Pages;
 	private:
 		string GetProjectName();
+		void CreateProjectDir();
 };
 
 
@@ -18,19 +17,35 @@ Project::Project(char* url)
 	
 	Project::url = url;
 	Project::name = Project::GetProjectName();
+
+	Project::CreateProjectDir();
 	Project::Pages.push_back(new Page(url));
 }
 
-
+/* Parse url and return site name */
 string Project::GetProjectName()
 {
 	cmatch _match;
 
-	regex_match(url.c_str(), _match, regex("(https://|http://)www.?(.*)"));
+	regex_match(url.c_str(), _match, regex("[a-z:/]+/([a-z.]+)[a-z./]+"));
 
-	for( int i = 0; i < _match.size(); i++ ){
-		cout << ": " << _match[i] << endl;	
-	}
-
-	return url;	
+	return _match[1];	
 }
+
+/* Check and create project root directory */
+void Project::CreateProjectDir()
+{
+	int status = mkdir(name.c_str(), S_IRWXU);
+
+	if( status == -1 ) {
+		switch(errno) {
+			case 17:
+				cerr << "Directory already exist." << endl;
+				break;
+			default:
+				throw "Can't create directory.";
+		}
+	}
+}
+
+
